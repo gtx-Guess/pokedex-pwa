a8ad4ba1efcd9444d3ae05d500c13e66c7679576
gtx-Guess <tigranalpha@gmail.com>
2025-07-23 09:25:52 -0700

    Fixing pwa configuration

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,64 @@
-self.__wb_disable_dev_logs = true;
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-workbox.setConfig({ debug: false, logLevel: 'warn' });
-workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
-);
+const CACHE_NAME = 'pokedex-pwa-v1';
+const OFFLINE_URL = '/';
+
+// Files to cache for offline functionality
+const CACHE_FILES = [
+    '/',
+    '/manifest.json',
+    '/pokeball.svg',
+    '/icons/poke-icon.svg'
+];
+
+// Install event - cache essential files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('SW: Cache opened');
+                return cache.addAll(CACHE_FILES);
+            })
+            .catch((error) => {
+                console.log('SW: Cache failed', error);
+            })
+    );
+    self.skipWaiting();
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('SW: Deleting old cache', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => {
+                    return caches.open(CACHE_NAME)
+                        .then((cache) => {
+                            return cache.match(OFFLINE_URL);
+                        });
+                })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then((response) => {
+                    return response || fetch(event.request);
+                })
+        );
+    }
+});
